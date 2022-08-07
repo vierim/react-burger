d46a460cca9aa97b3ae1aeb51151cd522cc01628
web77code <web77@yandex.ru>
2022-08-07 20:10:50 +0300

    feat: add tipization to helpers func

---
+++ src/utils/helpers.ts
@@ -0,0 +1,31 @@
+import { TIngredient, TOrder } from "./types";
+
+export { calculateOrderCost, compareOrdersDate };
+
+function calculateOrderCost(
+  orderIdList: string[],
+  ingredientsData: Array<TIngredient>
+): number {
+  return orderIdList.reduce((prev, item) => {
+    const currentIngredient = ingredientsData.find(
+      (el: TIngredient) => el._id === item
+    );
+
+    if (typeof currentIngredient !== "undefined") {
+      return prev + currentIngredient.price;
+    } else {
+      return prev;
+    }
+  }, 0);
+}
+
+function compareOrdersDate(a: TOrder, b: TOrder) {
+  const x = new Date(a.updatedAt).getTime();
+  const y = new Date(b.updatedAt).getTime();
+
+  if (x > y) {
+    return -1;
+  } else if (x < y) {
+    return 1;
+  } else return 0;
+}
