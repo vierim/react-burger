05ac36158b7438b83e332b292816f39adcae1c60
web77code <web77@yandex.ru>
2022-08-16 09:04:21 +0300

    feat: add types to ingredient-details component

---
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from '../../services/store';
+
+import { TIngredient } from '../../types';
+import { getDataThunk } from '../../services/actions/burger-ingredients/thunks';
+
+import styles from './ingredient-details.module.css';
+
+interface IIngredientDetailsProps {
+  header: string;
+}
+
+interface IIngredientParams {
+  id: string;
+}
+
+const IngredientDetails = (props: IIngredientDetailsProps) => {
+  const { header } = props;
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const { id } = useParams<IIngredientParams>();
+  const ingredients = useSelector((state) => state.ingredients.data);
+  const [state, setState] = useState<TIngredient | undefined>(undefined);
+
+  useEffect(() => {
+    if (!ingredients.length && !location.state) {
+      dispatch(getDataThunk());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (ingredients.length !== 0) {
+      const data = ingredients.find((el) => el._id === id);
+
+      setState(data ? data : undefined);
+    }
+  }, [ingredients, id]);
+
+  return (
+    <>
+      {state && (
+        <div className={styles.container}>
+          {header && (
+            <h2 className="mt-30 text text_type_main-large">{header}</h2>
+          )}
+          <div className={styles.card}>
+            <img
+              className={styles.image + ' mb-4'}
+              src={state.image_large}
+              alt={state.name}
+            />
+            <h3 className={styles.name + ' mb-8 text text_type_main-medium'}>
+              {state.name}
+            </h3>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <h4 className="text text_type_main-default">Калории,ккал</h4>
+                <p className="text text_type_digits-default">
+                  {state.calories}
+                </p>
+              </li>
+              <li className={styles.item}>
+                <h4 className="text text_type_main-default">Белки, г</h4>
+                <p className="text text_type_digits-default">
+                  {state.proteins}
+                </p>
+              </li>
+              <li className={styles.item}>
+                <h4 className="text text_type_main-default">Жиры, г</h4>
+                <p className="text text_type_digits-default">{state.fat}</p>
+              </li>
+              <li className={styles.item}>
+                <h4 className="text text_type_main-default">Углеводы, г</h4>
+                <p className="text text_type_digits-default">
+                  {state.carbohydrates}
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default IngredientDetails;
