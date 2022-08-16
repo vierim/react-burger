4b579b4c613b6e76135b08c937e09c7a0f8c4517
web77code <web77@yandex.ru>
2022-08-16 08:26:46 +0300

    feat: add custom hooks useSelector and useDispatch with types

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,10 +1,16 @@
-import thunk from "redux-thunk";
-import { compose, createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import { compose, createStore, applyMiddleware } from 'redux';
+import {
+  TypedUseSelectorHook,
+  useDispatch as dispatchHook,
+  useSelector as selectorHook,
+} from 'react-redux';
 
-import { rootReducer } from "./reducers";
-import socketMiddleware from "./middleware";
+import { RootState, AppDispatch, AppThunk } from './types';
+import { rootReducer } from './reducers';
+import socketMiddleware from './middleware';
 
-import { wsActions } from './actions/orders/constants'
+import { wsActions } from './actions/orders/constants';
 
 declare global {
   interface Window {
@@ -12,12 +18,15 @@ declare global {
   }
 }
 
-export const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancers(
   applyMiddleware(socketMiddleware(wsActions), thunk)
 );
 
 const store = createStore(rootReducer, enhancer);
-
 export default store;
+
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+export const useDispatch = () => dispatchHook<AppDispatch | AppThunk>();
