d7673b59405575f8a7eaaca94e5120f594dea098
web77code <web77@yandex.ru>
2022-08-16 10:10:54 +0300

    feat: add types to modal-overlay component

---
+++ src/components/modal-overlay/interface.ts
@@ -0,0 +1,3 @@
+export interface IModalOverlayProps {
+  closeModal: () => void;
+}
--- src/components/modal-overlay/modal-overlay.tsx
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -1,16 +1,13 @@
-import PropTypes from 'prop-types';
+import { IModalOverlayProps } from './interface';
 
 import styles from './modal-overlay.module.css';
 
-const ModalOverlay = ({closeModal}) => {
+const ModalOverlay = (props: IModalOverlayProps) => {
+  const {closeModal} = props;
 
   return (
     <div className={styles.overlay} onClick={closeModal}></div>
   );
 }
-
-ModalOverlay.propTypes = {
-  closeModal: PropTypes.func.isRequired,
-};
 
 export default ModalOverlay;
