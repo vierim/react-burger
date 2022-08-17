592b7d42b80ec03e16940bf58cf37d4702defd45
web77code <web77@yandex.ru>
2022-08-17 08:10:26 +0300

    style: code style in start-prompting component

--- src/components/start-prompting/start-prompting.tsx
+++ src/components/start-prompting/start-prompting.tsx
@@ -1,10 +1,11 @@
 const StartPrompting = () => {
-
   return (
     <div className="mt-15 mb-15">
-      <p className="text text_type_main-medium">Выберите булку для бургера, чтобы начать создавать заказ.</p>
+      <p className="text text_type_main-medium">
+        Выберите булку для бургера, чтобы начать создавать заказ.
+      </p>
     </div>
   );
-}
+};
 
 export default StartPrompting;
