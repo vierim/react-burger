0ae4d8ca2ae31788c23b54c6f331b1f0353eeafe
web77code <web77@yandex.ru>
2022-08-18 10:13:03 +0300

    feat: add types to forgot-password page

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,35 +1,37 @@
-import { useState } from "react";
-import { Redirect, Link, useHistory } from "react-router-dom";
+import React, { useState } from 'react';
+import { Redirect, Link, useHistory } from 'react-router-dom';
+
+import { TUserEmail } from '../../types';
+
+import { resetUserPassword } from '../../utils/api';
+import { logErrorToConsole } from '../../utils/utils';
+
+import AnimatedLoader from '../../components/animated-loader';
 import {
   Input,
   Button,
-} from "@ya.praktikum/react-developer-burger-ui-components";
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
-import { resetUserPassword } from "../../utils/api";
-import { logErrorToConsole } from "../../utils/utils";
-
-import AnimatedLoader from "../../components/animated-loader";
-
-import styles from "./forgot-password.module.css";
+import styles from './forgot-password.module.css';
 
 const ForgotPassword = () => {
   const history = useHistory();
 
-  const [email, setEmail] = useState("");
-  const [response, setResponse] = useState(false);
-  const [process, setProcess] = useState(false);
+  const [email, setEmail] = useState<string>('');
+  const [response, setResponse] = useState<boolean>(false);
+  const [process, setProcess] = useState<boolean>(false);
 
-  const onChange = (e) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (email.length > 6) {
       setProcess(true);
 
-      resetUserPassword(email)
+      resetUserPassword({ email } as TUserEmail)
         .then((res) => {
           if (res.success) {
             setProcess(false);
@@ -41,7 +43,7 @@ const ForgotPassword = () => {
         })
         .finally(() => {
           setProcess(false);
-        })
+        });
     }
   };
 
@@ -52,7 +54,7 @@ const ForgotPassword = () => {
       {response ? (
         <Redirect
           to={{
-            pathname: "/reset-password",
+            pathname: '/reset-password',
             state: { from: history.location.pathname },
           }}
         />
@@ -61,17 +63,17 @@ const ForgotPassword = () => {
           <h1 className="mb-6 text text_type_main-medium">
             Восстановление пароля
           </h1>
-          <form className={"mb-20 " + styles.form} onSubmit={onSubmit}>
-            <div className={"mb-6 " + styles.input}>
+          <form className={'mb-20 ' + styles.form} onSubmit={onSubmit}>
+            <div className={'mb-6 ' + styles.input}>
               <Input
-                type={"text"}
-                placeholder={"Укажите email"}
+                type={'text'}
+                placeholder={'Укажите email'}
                 onChange={onChange}
                 value={email}
                 error={false}
-                errorText={"Ошибка"}
-                name={"name"}
-                size={"default"}
+                errorText={'Ошибка'}
+                name={'name'}
+                size={'default'}
               />
             </div>
             <Button type="primary" size="medium">
@@ -79,7 +81,7 @@ const ForgotPassword = () => {
             </Button>
           </form>
           <p className="mb-4 text text_type_main-default text_color_inactive">
-            Вспомнили пароль?{" "}
+            Вспомнили пароль?{' '}
             <Link to="/login" className={styles.link}>
               Войти
             </Link>
